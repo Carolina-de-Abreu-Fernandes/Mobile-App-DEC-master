58e4dd472ecc15e2d7db8278af17a5aa8174c987
Carolina de Abreu Fernandes <carolinaabreu.f@gmail.com>
2020-12-07 02:31:46 -0300

    Dawn Update - 07/12/2020

--- src/Pages/Consume.js
+++ src/Pages/Consume.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Image } from 'react-native';
+import styled from 'styled-components';
+
+import Arrow from '../assets/img/arrow-down.png';
+import Dots from '../assets/img/vertical-dots.png';
+
+const Background = ({ children }) => {
+  return (
+    <LinearGradient
+      colors={['#1311', '#1121']}
+      style={{
+        flex: 1,
+        paddingTop: 50,
+      }}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
+const TobBar = styled.View`
+  flex-direction: row;
+`;
+
+TobBar.Left = styled.View`
+  flex: 1;
+  padding-left: 16px;
+`;
+TobBar.Middle = styled.View`
+  flex: 2;
+  align-items: center;
+`;
+TobBar.Right = styled.View`
+  flex: 1;
+  padding-right: 16px;
+  align-items: flex-end;
+`;
+
+TopBar.Title = styled.Text`
+  color: white;
+  text-transform: uppercase;
+`;
+TopBar.SubTitle = styled.Text`
+  color: white;
+  font-weight: bold;
+`;
 
 export function Consume() {
   return (
-    <View style={styles.container}>
-      <Text> Tela de Gastos Mensais</Text>
-    </View>
+    <Background>
+      <TopBar>
+        <TopBar.Left>
+          <Image source={Arrow} style={{ width: 20, height: 20 }} />
+        </TopBar.Left>
+        <TopBar.Middle>
+          <TopBar.Title> Seus Gastos</TopBar.Title>
+          <TopBar.SubTitle> Mensais </TopBar.SubTitle>
+        </TopBar.Middle>
+        <TopBar.Right>
+          <Image source={Dots} style={{ width: 20, height: 20 }} />
+        </TopBar.Right>
+      </TopBar>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#111',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

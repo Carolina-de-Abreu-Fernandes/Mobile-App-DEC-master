2f2df20e9fce494bccd137ec65fee12393a80d3f
Carolina de Abreu Fernandes <carolinaabreu.f@gmail.com>
2020-12-10 15:43:56 -0300

    Clean

--- App.js
+++ App.js
@@ -1,30 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
   View,
-  TextInput,
   Image,
+  KeyboardAvoidingView,
+  TextInput,
   TouchableOpacity,
-  Alert,
+  Text,
+  StyleSheet,
 } from 'react-native';
-
-import Login from './src/ScreenRegister';
-
-export default class appDEC extends Component {
-  render() {
-    return;
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>;
-  }
+import { Logo } from './src/assets/img/icon.png';
+export default function App() {
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.image}>
+        <Image source={Logo} />
+      </View>
+      <View styles={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TouchableOpacity>
+          <Text>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#151515',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 8,
   },
 });
-
-AppRegistry.registerComponent('appDEC', () => appDEC);

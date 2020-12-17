d03ee815b8877af4ce98d3f24bab5c1942a3b28b
Carolina de Abreu Fernandes <carolinaabreu.f@gmail.com>
2020-12-17 03:46:49 -0300

    Final no Final

--- src/Pages/Consume.js
+++ src/Pages/Consume.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-
-import { Image, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { Text, View, Image, StyleSheet, KeyboardAvoidingView } from 'react-native';
 import styled from 'styled-components';
 import { Button } from 'react-native-elements';
+import { Path } from 'react-native-svg';
 import Icon from 'react-native-vector-icons/Feather';
+import { Grid, LineChart, XAxis, YAxis, ProgressCircle } from 'react-native-svg-charts';
+import * as shape from 'd3-shape';
 
 const Background = ({ children }) => {
   return (
@@ -47,7 +49,41 @@ TopBar.SubTitle = styled.Text`
   text-transform: uppercase;
 `;
 
-export default function Consume({ navigation }) {
+export default function Consume({ navigation }){
+
+  function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  }
+  throw new Error(response.statusText)
+}
+
+function json(response) {
+  return response.json()
+}
+fetch('http://private-anon-3943faed58-blynkapi.apiary-proxy.com/pRUnjW4a-As-hGkUM4IdcupQRMgr-JnN/project', {
+  method: 'get',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name: 'Hubot',
+    login: 'hubot',
+  })
+}).then(status)
+  .then(json)
+  .then(function(json) {
+    console.log('request succeeded with json response', json)
+  }).catch(function(error) {
+    console.log('request failed', error)
+  })
+
+         const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
+
+        const axesSvg = { fontSize: 10, fill: 'grey' };
+        const verticalContentInset = { top: 10, bottom: 10 };
+        const xAxisHeight = 30;
   return (
     <Background>
       <TopBar>
@@ -75,6 +111,76 @@ export default function Consume({ navigation }) {
           />
         </TopBar.Right>
       </TopBar>
+<View>
+ <Text style={{ paddingLeft:30, paddingTop: 10, paddingBottom: 10, color: '#fffceb'}}>
+            Consumo de Energia (kWh)
+            </Text>
+</View>
+             <View style={{ height: 230, padding: 25, flexDirection: 'row' }}>
+                <YAxis
+                    data={data}
+                    style={{ marginBottom: xAxisHeight }}
+                    contentInset={verticalContentInset}
+                    svg={axesSvg}
+                />
+                <View style={{ flex: 1, marginLeft: 10 }}>
+                    <LineChart
+                        style={{ flex: 1 }}
+                        data={data}
+                        contentInset={verticalContentInset}
+                        svg={{ stroke: '#55a630' }}
+                    >
+                        <Grid/>
+                    </LineChart>
+                    <XAxis
+                        style={{ marginHorizontal: -10, height: xAxisHeight }}
+                        data={data}
+                        formatLabel={(value, index) => index}
+                        contentInset={{ left: 10, right: 10 }}
+                        svg={axesSvg}
+                    />
+                </View>
+            </View>
+
+            <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={styles.none}>
+            <Text style={styles.title}>
+            Corrente (A)
+            </Text>
+            <ProgressCircle
+                style={ { height: 60, flex: 1, flexDirection: 'row', marginBottom: 45  } }
+                progress={ 0.1 }
+                progressColor={'#ffa200'}
+                startAngle={0}
+                endAngle={Math.PI * 3.2}
+            />
+            </View>
+            <View style={styles.none}>
+            <Text style={styles.title}>
+            Potência (W)
+            </Text>
+            <ProgressCircle
+                style={ { height: 60,flex: 1, flexDirection: 'row', marginBottom: 45 } }
+                progress={ 0.1 }
+                progressColor={'#1e96fc'}
+                startAngle={ 0 }
+                endAngle={ Math.PI * 3.2 }
+            />
+             </View>
+            </View>
     </Background>
   );
 }
+
+const styles = StyleSheet.create({
+ title: {
+   flex: 1,
+   color: '#fffceb',
+   justifyContent: 'center',
+   paddingLeft:40
+  },
+  none: {
+    flex:1,
+    padding: 10,
+  },
+})
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL:
+    'http://private-anon-3943faed58-blynkapi.apiary-proxy.com/pRUnjW4a-As-hGkUM4IdcupQRMgr-JnN/project',
+});
+
+export default api;
